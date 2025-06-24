0b91d74f6d47c92b63cd874f6d5ddc1cc85de46c
Asha K <asha.kothandan@gmail.com>
2025-06-24 11:50:49 +0530

    update settings

--- pro/settings.py
+++ pro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eu)nlk6095f20x9vj)l(bdpj$!t7zxegz2p-gkv^#ya8d%hjy7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
